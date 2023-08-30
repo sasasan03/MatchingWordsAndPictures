cf23c18201fe0be9c91f73eb134df7f24e021a46
sasasan03 <sako.hiro62@gmail.com>
2023-08-30 20:48:46 +0900

    first

---
+++ MatchingWordsAndPictures/MatchingWordsAndPicturesApp.swift
@@ -0,0 +1,18 @@
+//
+//  MatchingWordsAndPicturesApp.swift
+//  MatchingWordsAndPictures
+//
+//  Created by sako0602 on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct MatchingWordsAndPicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(soundType: SoundType.bibin)
+//            AVFoundationTest()
+        }
+    }
+}
